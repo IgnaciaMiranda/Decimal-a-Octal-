ed5d87fe6fed29129baae3a2f813dd800aa1f41b
Ignacia <166965387+IgnaciaMiranda@users.noreply.github.com>
2025-03-28 22:08:57 -0300

    Update DeciamlOctal.c

--- DeciamlOctal.c
+++ DeciamlOctal.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 
 int main(){
-    int decimal, resto, i=0;
+    int decimal, resto, i = 0;
     char octal[20];
-    printf("Ingresa un numero:");
+
+    printf("Ingresa un número: ");
     scanf("%d", &decimal);
 
-    if(decimal == 0){
-        printf("0/n");
+    if (decimal == 0) {
+        printf("0\n");
         return 0;
-
     }
 
-    while (decimal > 0){
+    while (decimal > 0) {
         resto = decimal % 8;
         octal[i++] = resto + '0';
         decimal /= 8;
     }
 
-    for(int j=i-1; j>=0;j--){
-        printf("%c, octal[j]");
-
+    for (int j = i - 1; j >= 0; j--) {
+        printf("%c", octal[j]);
     }
-    printf("/n");
+
+    printf("\n");
 
     return 0;
 }
