230ec7b6d5d475cd23da9c4ce3f5fc8d2c4b999d
IgnaciaMiranda <166965387+IgnaciaMiranda@users.noreply.github.com>
2025-03-28 21:59:39 -0300

    Decimal a octal
    Convierte un número decimal a su equivalente en octal.
    
    Solicita al usuario que ingrese un número entero en base 10.
    
    Calcula su equivalente en base 8 (octal) dividiendo sucesivamente entre 8 y guardando los residuos.
    
    Muestra el número en octal por pantalla.

---
+++ DeciamlOctal.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+    int decimal, resto, i=0;
+    char octal[20];
+    printf("Ingresa un numero:");
+    scanf("%d", &decimal);
+
+    if(decimal == 0){
+        printf("0/n");
+        return 0;
+
+    }
+
+    while (decimal > 0){
+        resto = decimal % 8;
+        octal[i++] = resto + '0';
+        decimal /= 8;
+    }
+
+    for(int j=i-1; j>=0;j--){
+        printf("%c, octal[j]");
+
+    }
+    printf("/n");
+
+    return 0;
+}
